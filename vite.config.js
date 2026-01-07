96e1bf893278623dd09d5082f515c4b13934f780
Henrique Cícero <henriquecicero@gmail.com>
2026-01-07 07:54:45 -0300

    ci: setup github pages

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import mdx from '@mdx-js/rollup'
-import remarkFrontmatter from 'remark-frontmatter'
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
-import rehypeHighlight from 'rehype-highlight'
+import mdx from '@mdx-js/rollup';
+import react from '@vitejs/plugin-react';
+import rehypeHighlight from 'rehype-highlight';
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/',
   plugins: [
     react(),
     mdx({
-      remarkPlugins: [
-        remarkFrontmatter,
-        remarkMdxFrontmatter,
-      ],
-      rehypePlugins: [
-        [rehypeHighlight, { detect: true, ignoreMissing: true }],
-      ],
+      remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+      rehypePlugins: [[rehypeHighlight, { detect: true, ignoreMissing: true }]],
     }),
   ],
-})
+});
