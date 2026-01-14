a9330ed1c835dc58f4968099ab33bdba797eb543
Henrique Cícero <henriquecicero@gmail.com>
2026-01-13 23:03:32 -0300

    feat: add logic to handle post page titles in Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -24,6 +24,9 @@ function Layout({ children }: LayoutProps) {
   // Build alternate URL (for hreflang)
   const alternateUrl = currentUrl; // Same URL, different language
 
+  // Check if we're on a post page
+  const isPostPage = location.pathname.startsWith('/posts/');
+
   // Set HTML lang attribute
   useEffect(() => {
     document.documentElement.lang = langUtils.htmlLang;
@@ -120,7 +123,8 @@ function Layout({ children }: LayoutProps) {
         ];
 
         return {
-          title: t('site.title'),
+          // Don't set title on post pages - let Post component handle it
+          ...(isPostPage ? {} : { title: t('site.title') }),
           meta: metaTags,
           link: linkTags,
           script: structuredData.map((data, index) => ({
@@ -130,7 +134,7 @@ function Layout({ children }: LayoutProps) {
           })),
         };
       },
-      [t, currentUrl, langUtils, alternateUrl, structuredData, location.pathname]
+      [t, currentUrl, langUtils, alternateUrl, structuredData, location.pathname, isPostPage]
     )
   );
 
